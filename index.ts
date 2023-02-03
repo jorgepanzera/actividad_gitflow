6b066bbe7593f70164d627fef4c5e256bd0854b5
Jorge Panzera <panzera.jorge@gmail.com>
2023-02-02 21:46:15 -0300

    Feature 2 se agrego Clase B #2

--- index.ts
+++ index.ts
@@ -6,5 +6,15 @@ interface Person {
 
 class classA implements Person { 
    foo: string;
-   bar: number
+   bar: number;
+   name: string;
+   age: number;
+   profession: string;   
+}
+
+class classB {
+
+    sum = (x: number, y: number): number => {
+        return x + y;
+    }
 }
